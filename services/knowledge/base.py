ef439604db41bc3a1798a57ed602418719f58579
Andrew Ruan <aruan.careers@gmail.com>
2026-03-31 09:08:18 -0400

    fix: Flush last batch (#111)
    * flush last batch after processign finishes
    
    * store should stop based on prev 1 processes
    
    * lint

--- services/knowledge/base.py
+++ services/knowledge/base.py
@@ -178,6 +178,11 @@ class KnowledgeService(ABC):
                 handler=handler,
                 should_exit=should_exit
             )
+
+            # flushes last batch that hangs in memory, due to not reaching batch size.
+            if handler.item_list:
+                handler.flush()
+
             count = worker.message_count
         except Exception:
             self.logger.exception("Error during processing for queue: %s. (%s)",
@@ -224,7 +229,7 @@ class KnowledgeService(ABC):
 
         def should_exit(drained_any: bool) -> bool:
             # process is done, AND check processed queue was empty this iteration
-            return self._process_done.is_set() and not drained_any
+            return self._ingest_done.is_set() and self._process_done.is_set() and not drained_any
 
         count = 0
 
