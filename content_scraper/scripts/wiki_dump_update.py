9b19a03b1c4fed14b00ece28b0208850e39198f6
im-muhammed <Muhammed.Imran@ssc-spc.gc.ca>
2026-04-09 10:40:53 -0400

    Lint fix!!!!

--- content_scraper/scripts/wiki_dump_update.py
+++ content_scraper/scripts/wiki_dump_update.py
@@ -66,7 +66,7 @@ def list_maker(wiki_dump_content_url, wiki_dump_index_url):
     index_download_a = indexsoup.find("a", href=True).get("href")
 
     # For testing, the content file is very large, so you may comment out the below line to only download the index file
-    # link_list.append(content_download_a)
+    link_list.append(content_download_a)
     link_list.append(index_download_a)
     return link_list
 
