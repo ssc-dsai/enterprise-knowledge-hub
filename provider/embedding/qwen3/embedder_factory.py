fe83907bc9ba671c179ea5a414aaa8f1c4c6b6d7
Perlina Ly <119460770+perlinaly@users.noreply.github.com>
2026-04-15 10:10:57 -0400

    Removing llama-ccp (#130)

--- provider/embedding/qwen3/embedder_factory.py
+++ provider/embedding/qwen3/embedder_factory.py
@@ -1,13 +1,8 @@
 """Factory to choose embedder"""
-import os
 from functools import lru_cache
 
 @lru_cache(maxsize=1)
 def get_embedder():
-    """get embedder type based on env os"""
-    if os.getenv("WIKIPEDIA_EMBEDDING_MODEL_BACKEND", "LLAMA").upper() == "SENTENCE_TRANSFORMER":
-        from provider.embedding.qwen3.sentence_transformer import Qwen3SentenceTransformer # pylint: disable=import-outside-toplevel
-        return Qwen3SentenceTransformer()
-
-    from provider.embedding.qwen3.llama_embed import Qwen3LlamaCpp # pylint: disable=import-outside-toplevel
-    return Qwen3LlamaCpp()
+    """get SentenceTransformer embedder"""
+    from provider.embedding.qwen3.sentence_transformer import Qwen3SentenceTransformer # pylint: disable=import-outside-toplevel
+    return Qwen3SentenceTransformer()
